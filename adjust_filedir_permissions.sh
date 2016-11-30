384301aae9a636fd50b47cf12293106e014513dc
Jack Szwergold <JackSzwergold@gmail.com>
2016-11-30 10:41:34 -0500

    Adjusting the 'adjust_filedir_permissions.sh' for Drupal directory ownership issues.

--- adjust_filedir_permissions.sh
+++ adjust_filedir_permissions.sh
@@ -90,13 +90,15 @@ if mkdir ${LOCK_DIR} 2>/dev/null; then
       find ${FULL_DIRECTORY_PATH} -type f ! -perm ${CHMOD_READ_FILE} -iwholename '*settings.php' -print0 | xargs --no-run-if-empty -0 chmod ${CHMOD_READ_FILE} >/dev/null & FILE_PERM_FIX_PID=(`jobs -l | awk '{print $2}'`);
       wait ${FILE_PERM_FIX_PID};
 
+      # Adjust permissions for the Drupal settings file.
       find ${FULL_DIRECTORY_PATH} -type d ! -perm ${CHMOD_DRUPAL_DIR} -iwholename '*sites/default' -print0 | xargs --no-run-if-empty -0 chmod ${CHMOD_DRUPAL_DIR} >/dev/null & FILE_PERM_FIX_PID=(`jobs -l | awk '{print $2}'`);
       wait ${FILE_PERM_FIX_PID};
 
-      # Adjust permissions for files in directories named 'bin/'.
-      # find ${FULL_DIRECTORY_PATH} -type d \( -name "bin" -o -name "cgi-bin" \)` -maxdepth 1 -type f -print0 | xargs --no-run-if-empty -0 ls -la
-      # find ${FULL_DIRECTORY_PATH} -type d \( -name "bin" -o -name "cgi-bin" \)` -maxdepth 1 -type f -print0 | xargs --no-run-if-empty -0 chmod -R ${CHMOD_EXEC_FILE} >/dev/null & FILE_PERM_FIX_PID=(`jobs -l | awk '{print $2}'`);
-      # wait ${FILE_PERM_FIX_PID};
+      # Adjust ownership for specific Drupal directories.
+      find ${FULL_DIRECTORY_PATH} ! -user ${CHGRP_USER} -iwholename '*sites/default' -print0 | xargs --no-run-if-empty -0 chown ${CHGRP_USER} >/dev/null & USER_FIX_PID=(`jobs -l | awk '{print $2}'`);
+      wait ${USER_FIX_PID};
+      find ${FULL_DIRECTORY_PATH} ! -user ${CHGRP_USER} -iwholename '*sites/all' -print0 | xargs --no-run-if-empty -0 chown ${CHGRP_USER} >/dev/null & USER_FIX_PID=(`jobs -l | awk '{print $2}'`);
+      wait ${USER_FIX_PID};
 
     fi
 
